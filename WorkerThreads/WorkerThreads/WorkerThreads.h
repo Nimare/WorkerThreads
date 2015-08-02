d28331c1147cd25b9d5c073d974c5f06e0d158e8
Nimare <gyorkimilan@gmail.com>
2015-08-02 19:20:43 +0200

    Constructor for workerthreads

--- WorkerThreads/WorkerThreads/WorkerThreads.h
+++ WorkerThreads/WorkerThreads/WorkerThreads.h
@@ -1,11 +1,23 @@
 #pragma once
 
-template <class Item>
+#include <vector>
+#include <thread>
+
 class WorkerThread
 {
 public:
-	WorkerThread(Item & it): workItem(it) {};
+	WorkerThread(): WorkerThread(std::thread::hardware_concurrency()) {};
+	WorkerThread(unsigned int numberOfThreads) : m_threadPool()
+	{
+		for (unsigned int i; i < numberOfThreads; ++i)
+		{
+			m_threadPool.emplace_back(std::thread(&WorkerThread::threadWrokerLoop, this));
+		}
+	}
 	~WorkerThread() {};
 private:
-	Item & workItem;
+	void threadWrokerLoop()
+	{
+	}
+	std::vector<std::thread> m_threadPool;
 };
